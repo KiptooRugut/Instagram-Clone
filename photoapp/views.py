953bfea0e1b18c52e10487a1625e828e7128ac45
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-05 01:34:28 +0300

    it will add the newsletter view function and fix errors

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http  import HttpResponse
+import photoapp
+from .forms import NewsLetterForm
+from .email import send_welcome_email
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
-from django.urls import reverse
 import datetime as dt
-from .models import Comment, Photos, Profile
+from .models import Comment, NewsLetterRecipients, Photos, Profile
 from .forms import (CommentForm, CreateUserForm, NewPost, UserUpdate, ProfileForm)
 
 
@@ -74,26 +76,29 @@ def profile(request):
             'user_form':user_form,
             'profile_form': profile_form
         }
-
-    return render(request, 'all-photoapp/profile.html', display)
+    
+        return render(request, 'all-photoapp/profile.html',display)
+    return HttpResponseRedirect(request.path_info) 
 
 @login_required(login_url='/accounts/login/')
-def new_post(request):
+def newpost(request):
     current_user=request.user
     user_profile=Profile.objects.filter(user=current_user)
     if request.method=='POST':
         form = NewPost(request.POST,request.FILES)
         if form.is_valid():
-            title=form.cleaned_data.get('title')
-            image=form.cleaned_data.get('image')
-            captions=form.cleaned_data.get('post')
-            user=form.cleaned_data.get('user')
-            post = Photos(title=title, image=image, post=captions,user=user)
-            post.save()
+            data=form.save(commit=False)
+            data.save()
+            # title=form.cleaned_data.get('title')
+            # image=form.cleaned_data.get('image')
+            # captions=form.cleaned_data.get('post')
+            # user=form.cleaned_data.get('user')
+            # post = Photos(title=title, image=image, post=captions,user=user)
+            # post.save()
         else:
             print(form.errors)
         
-        return redirect('home')
+        return HttpResponseRedirect(request.path_info)
     else:
         form=NewPost()
     return render(request,'all-photoapp/post.html',{'form':form})
@@ -107,9 +112,8 @@ def details(request,image_id):
     return render(request, 'all-photoapp/details.html', {"image": image})
 
 def search_results(request):
-    
-    if 'Photo' in request.GET and request.GET["Photo"]:
-        search_term = request.GET.get("Photo")
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
         print(search_term)
         searched_results = Photos.search(search_term)
         message = f"{search_term}"
@@ -117,7 +121,23 @@ def search_results(request):
         return render(request, 'all-photoapp/search.html',{"message":message,"results": searched_results})
 
     else:
+        print('Say Chesee')
         message = "You haven't searched for any term"
         return render(request, 'all-photoapp/search.html',{"message":message})
 
+from .email import send_welcome_email
+def newsletter(request):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('')
+            #.................
+    return render(request, 'all-photoapp/home.html', {"date": date,"photoapp":photoapp,"letterForm":form})
 
