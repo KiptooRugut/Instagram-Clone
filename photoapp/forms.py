94ebcb6f418f2c73cd50011e4273993faa309c5b
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-05 01:37:35 +0300

    it will add newsletter form

--- photoapp/forms.py
+++ photoapp/forms.py
@@ -40,6 +40,11 @@ class CommentForm(forms.ModelForm):
         
     class Meta:
         model=Comment
-        fields = ('comment',) 
+        fields = ('comment',)\
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+ 
         
        
