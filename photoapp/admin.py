0717b264f335d9fda7e4fe6a2fe796176132585e
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-04 13:46:39 +0300

    it will add models to our Admin page using the admin.site.register() functions.

--- photoapp/admin.py
+++ photoapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Photos,tags
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags')
+
+admin.site.register(Profile) 
+admin.site.register(Photos) 
+admin.site.register(tags)
