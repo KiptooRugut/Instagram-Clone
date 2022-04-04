8326883b3de24aead8412f3207c67756811fc10c
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-05 01:39:15 +0300

    it will add django registration backend pathway

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path,include
+# from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('photoapp.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    # path('logout/', views.logout, {"next_page": '/'}),
 ]
